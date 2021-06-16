faaa5395612dec26bb804a65eacb93cc2047dfdb
DanielAristy <danielaristy22@gmail.com>
2021-06-15 22:12:58 -0500

    Se crean los eventos restantes para cada uno de los comportamientos de este agregado Canal

--- src/main/java/canal/Canal.java
+++ src/main/java/canal/Canal.java
@@ -1,11 +1,16 @@
 package canal;
 
 import canal.events.CanalCreado;
+import canal.events.CertificadoCambiado;
+import canal.events.NombreCambiado;
+import canal.events.TelefonoCambiado;
 import canal.values.CanalId;
 import canal.values.Certificado;
 import canal.values.Nombre;
 import canal.values.Telefono;
 import co.com.sofka.domain.generic.AggregateEvent;
+
+import java.util.Objects;
 
 public class Canal extends AggregateEvent<CanalId> {
     protected Nombre nombre;
@@ -15,5 +20,31 @@ public class Canal extends AggregateEvent<CanalId> {
         super(entityId);
         //Evento de dominio
         appendChange(new CanalCreado(nombre,certificado,telefono)).apply();
+    }
+
+    public void cambiarNombre(Nombre nombre){
+        Objects.requireNonNull(nombre);
+        appendChange(new NombreCambiado(nombre)).apply();
+    }
+
+    public void cambiarTelefono(Telefono telefono){
+        Objects.requireNonNull(telefono);
+        appendChange(new TelefonoCambiado(telefono)).apply();
+    }
+    public void cambiarCertificado(Certificado certificado){
+        Objects.requireNonNull(certificado);
+        appendChange(new CertificadoCambiado(certificado)).apply();
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public Certificado certificado() {
+        return certificado;
+    }
+
+    public Telefono telefono() {
+        return telefono;
     }
 }
