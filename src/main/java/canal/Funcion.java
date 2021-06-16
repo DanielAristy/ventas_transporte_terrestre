b1d6ab94adb610c40db4249e6ee4a5b6e65274a0
DanielAristy <danielaristy22@gmail.com>
2021-06-15 22:28:04 -0500

    Agregando comportamientos de la entidad Funcion y la validacion de cada uno de sus objetos

--- src/main/java/canal/Funcion.java
+++ src/main/java/canal/Funcion.java
@@ -1,4 +1,34 @@
 package canal;
 
-public class Funcion {
+import canal.values.Caracteristica;
+import canal.values.Descripcion;
+import canal.values.FuncionID;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Funcion extends Entity<FuncionID> {
+    private Caracteristica caracteristica;
+    private Descripcion descripcion;
+    public Funcion(FuncionID entityId, Caracteristica caracteristica, Descripcion descripcion) {
+        super(entityId);
+        this.caracteristica = caracteristica;
+        this.descripcion = descripcion;
+    }
+
+    public void actualizarCaracteristica(Caracteristica caracteristica){
+        this.caracteristica = Objects.requireNonNull(caracteristica);
+    }
+
+    public void actualizarDescripcion(Descripcion descripcion){
+        this.descripcion = Objects.requireNonNull(descripcion);
+    }
+
+    public Caracteristica caracteristica() {
+        return caracteristica;
+    }
+
+    public Descripcion descripcion() {
+        return descripcion;
+    }
 }
